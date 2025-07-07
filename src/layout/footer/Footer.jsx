4d59153ef785cae0dea168654ec790ac63ef6bdc
Gaston Caza <gastoncazaubon@gmail.com>
2025-07-06 22:04:45 -0300

    Changes in names

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -10,13 +10,13 @@ const footer = () => {
 
 
                 <div class="contact">
-                    <div class="contactCard"><i class="fa-solid fa-comment"></i>&nbsp;<a href="https://wa.me/1163538689" target="_blank">whatsap</a></div>
-                    <div class="contactCard"><i class="fa-solid fa-phone"></i>&nbsp;<a href="tel:+5491163538689">phone</a>
+                    <div class="contactCard"><i class="fa-solid fa-comment"></i>&nbsp;<a href="https://wa.me/1163538689" target="_blank">Whatsapp</a></div>
+                    <div class="contactCard"><i class="fa-solid fa-phone"></i>&nbsp;<a href="tel:+5491163538689">Phone</a>
                     </div>
                     <div class="contactCard"><i class="fa-solid fa-envelope">&nbsp;</i><a
-                        href="mailto:gastoncazaubon@gmail.com?subject=Consulta Web?body=Te escribo porque te vi en la web">email</a>
+                        href="mailto:gastoncazaubon@gmail.com?subject=Consulta Web?body=Te escribo porque te vi en la web">Email</a>
                     </div>
-                    <div class="contactCard"><i class="fa-solid fa-globe">&nbsp;</i><a href="https://www.linkedin.com/in/gaston-cazaubon/" target="_blank">linkedin</a>
+                    <div class="contactCard"><i class="fa-solid fa-globe">&nbsp;</i><a href="https://www.linkedin.com/in/gaston-cazaubon/" target="_blank">LinkedIn</a>
                     </div>
 
                 </div>
